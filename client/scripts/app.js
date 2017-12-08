16f0a7072c485c4418aa5ae0dd06345d2eafb98e
Jessica Thompson <jthompson0670@gmail.com>
2017-12-08 14:44:25 -0800

    worked on frontend

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,10 @@
 // YOUR CODE HERE:
+// http://parse.sfm6.hackreactor.com/chatterbox/classes/messages
+
+let user = {
+	name = (prompt('What is your name?');
+}
+
+let app = {
+	init: function() {};
+};
