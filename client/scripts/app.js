ed0e40098e8a2f2f20c95d8443dbb04514a15d1b
Jessica Thompson <jthompson0670@gmail.com>
2017-12-08 20:52:00 -0800

    progress - need to access success: this.allMessages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,17 +4,31 @@
 // let user = {
 // 	// name = (prompt('What is your name?')
 // }
-
+      $(document).ready(() => {
+        // app.call(this);
+        app.fetch();
+        $(".chatUl").prepend('<li class="chat">' + message.username + ': ' + message.text + '</li>');
+        // $(".sendMessage").click(function(event) {
+          var newMessage = app.allMessages;
+          // if(newMessage.length > 0) {
+            $(".chatUl").prepend('<li class="chat">' + message.username + ': ' + newMessage + '</li>');
+            // this.send();
+            // app.send(newMessage);
+            // $('#chatInput').val("")
+          // }
+        // });
+        console.log(newMessage, 'test')
+      });
 
 let app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
 	init: function() {},
-  send: function() {
+  send: function(newMessage) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
-      data: JSON.stringify(message),
+      data: JSON.stringify(newMessage),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
@@ -30,10 +44,11 @@ let app = {
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
-      data: 'where={"username":"newhrsf86"}',
+      data: 'order=-createdAt',
       contentType: 'application/json',
       success: function (data) {
-        console.log(data);
+        this.allMessages = data;
+        console.log(this.allMessages)
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -43,9 +58,10 @@ let app = {
   },
   clearMessages: function() {
     $(".chat").html("");
-  }
+  },
+  allMessages: this.fetch()
 };
-app.fetch();
+// app.fetch();
 
 
 // POST request format
